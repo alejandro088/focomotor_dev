10b3b5e6a7b5e5dab5e0b540facc3797130f0b7b
Josias <67062870+JosiasEmanuelCastro@users.noreply.github.com>
2020-09-02 21:18:48 -0300

    Actualizacion header y nav mobile en todas las paginas y correcion de carrusel de imagenes

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -593,7 +593,7 @@
 
   var ARROW_RIGHT_KEYCODE = 39; // KeyboardEvent.which value for right arrow key
 
-  var TOUCHEVENT_COMPAT_WAIT = 500; // Time for mouse compat events to fire after touch
+  var TOUCHEVENT_COMPAT_WAIT = 0; // Time for mouse compat events to fire after touch
 
   var SWIPE_THRESHOLD = 40;
   var Default = {
