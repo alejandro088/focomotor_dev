1cf033146c3a6174ae510e8ab0f33c9d48b6b45e
Josias <67062870+JosiasEmanuelCastro@users.noreply.github.com>
2020-09-03 18:47:22 -0300

    publicar.html input de imagenes mejorado

--- js/focom-script.js
+++ js/focom-script.js
@@ -1,23 +1,83 @@
-var input = document.querySelector("#file-input");
 
-	document.querySelector("button").addEventListener("click", function () {
-	  input.click();
+/*
+
+	// DUDAS: https://developer.mozilla.org/es/docs/Web/API/FileReader/readAsDataURL
+	var imageInputOne = document.querySelector("#focom-image-input-one"); // Toma el input image
+	var imageChangeOne = document.querySelector("#focom-image-preview-one"); // Toma el img preview
+	var imageDeleteOne = document.getElementById('focom-image-delete-one'); // Toma el botton para eliminar
+
+	document.querySelector("#focom-button-input-one").addEventListener("click", function () {
+	  imageInputOne.click(); // Cuando se le hace click al boton de ejecuta el input image 
 	});
 
-	input.addEventListener("change", preview);
+	imageDeleteOne.addEventListener("click", function () {
+	 imageChangeOne.src = (' ');
+	 imageDeleteOne.className = (' ');
+	});
 
-	function preview() {
-	  var fileObject = this.files[0];
-	  var fileReader = new FileReader();
-	  fileReader.readAsDataURL(fileObject);
+	
+	imageInputOne.addEventListener("change", imagePreviewOne); // Cuando un archivo se suba en el input ejecuta la funcion imagePreview
 
-	  fileReader.onload = function () {
-	    var result = fileReader.result;
-	    var img = document.querySelector("#preview");
-	    img.setAttribute("src", result);
+	function imagePreviewOne() {
+	  var imageObjectOne = this.files[0]; // Toma el objeto de imagen cargado
+	  var imageReaderOne = new FileReader();
+	  imageReaderOne.readAsDataURL(imageObjectOne);
 
-
-	    var focomImageDelete = document.getElementById('focom-image-delete');
-	    focomImageDelete.className = "fas fa-times-circle fa-lg focom-image-delete text-danger";
+	  imageReader.onload = function () {
+	    var imageResultOne = imageReaderOne.result;
+	    imageChangeOne.src = (imageResultOne);
 	  };
+
+	  // Crear el boton de eliminar
+	  imageDeleteOne.className = "fas fa-times-circle focom-image-icon-delete text-danger";
 	}
+
+
+*/
+
+
+
+var inputImage = document.getElementById('focom-input-image');
+var inputButtom = document.getElementById('focom-input-buttom');
+var imageIconDelete = document.getElementById('focom-image-icon-delete');
+var imagePreview = document.getElementById('focom-image-preview');
+
+inputButtom.addEventListener("click", function () {
+	inputImage.click(); // Cuando se le hace click al boton de ejecuta el input image 
+});
+
+inputImage.addEventListener("change", previewImage);
+
+
+function previewImage() {
+	
+	var image = inputImage.files[0];
+
+	var imageReader = new FileReader;
+
+	imageReader.onloadend = function () {
+		imagePreview.src = imageReader.result;
+	}
+
+	if (image) {
+		imageReader.readAsDataURL(image);
+	} else {
+		imagePreview.src = ' ';
+	}
+
+	imageIconDelete.className = "fas fa-times-circle focom-image-icon-delete text-danger";
+	inputButtom.className = "d-none"
+}
+
+imageIconDelete.addEventListener("click", deleteImage);
+
+function deleteImage () {
+	imagePreview.src = ' ';
+	imageIconDelete.className = ' ';
+	inputButtom.className = ' ';
+}
+
+
+
+
+
